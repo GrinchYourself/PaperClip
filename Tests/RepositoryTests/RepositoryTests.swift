4f8c8114ca5834a00590c775203eb4c363e6cdbc
Olivier D <delaporteol@me.com>
2022-11-08 20:13:41 +0100

    increase RepositoryTests

--- Tests/RepositoryTests/RepositoryTests.swift
+++ Tests/RepositoryTests/RepositoryTests.swift
@@ -25,6 +25,7 @@ final class RepositoryTests: XCTestCase {
         cancellables.removeAll()
     }
 
+    //MARK: AdsRepository
     func testSuccessGetAdsListing() {
         let adsRemoteStoreDependency = MockAdsRemoteStore(isSucceeded: true)
         let categoriesRemoteStoreDependency = MockCategoriesRemoteStore(isSucceeded: true)
@@ -42,9 +43,138 @@ final class RepositoryTests: XCTestCase {
                 XCTFail("Failure not expected")
             }
         } receiveValue: { aggregatedAds in
-            XCTAssertEqual(3, aggregatedAds.0.count)
-            XCTAssertEqual(6, aggregatedAds.1.count)
-            //Continue to check values
+            // check ads
+            let ads = aggregatedAds.0
+            XCTAssertEqual(3, ads.count)
+            let ad = ads.first(where: { $0.id == 1691247255 })
+            XCTAssertNotNil(ad)
+            XCTAssertEqual(199, ad?.price)
+            XCTAssertNil(ad?.siret)
+            XCTAssertEqual("Pc portable hp elitebook 820 g1 core i5 4 go ram 250 go hdd", ad?.title)
+
+            //Check categories
+            let categories = aggregatedAds.1
+            XCTAssertEqual(6, categories.count)
+            let multimediaCategory = categories.first(where: { $0.id == 8 })
+            XCTAssertNotNil(multimediaCategory)
+            XCTAssertEqual("Multimédia", multimediaCategory?.name)
+
+        }.store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 0.5)
+    }
+
+    func testFailureGetAdsListingWhenAdsRemoteStoreFails() {
+        let adsRemoteStoreDependency = MockAdsRemoteStore(isSucceeded: false)
+        let categoriesRemoteStoreDependency = MockCategoriesRemoteStore(isSucceeded: true)
+
+        let adsRepository = AdsRepository(adsRemoteStore: adsRemoteStoreDependency,
+                                          categoriesRemoteStore: categoriesRemoteStoreDependency)
+
+        let expectation = XCTestExpectation(description: "get Listing Ads fails")
+
+        adsRepository.ads().sink { completion in
+            switch completion {
+            case .finished:
+                XCTFail("Success not expected")
+            case .failure:
+                expectation.fulfill()
+            }
+        } receiveValue: { _ in
+            XCTFail("Value not expected")
+        }.store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 0.5)
+    }
+
+    func testFailureGetAdsListingWhenCategoriesRemoteStoreFails() {
+        let adsRemoteStoreDependency = MockAdsRemoteStore(isSucceeded: true)
+        let categoriesRemoteStoreDependency = MockCategoriesRemoteStore(isSucceeded: false)
+
+        let adsRepository = AdsRepository(adsRemoteStore: adsRemoteStoreDependency,
+                                          categoriesRemoteStore: categoriesRemoteStoreDependency)
+
+        let expectation = XCTestExpectation(description: "get Listing Ads fails")
+
+        adsRepository.ads().sink { completion in
+            switch completion {
+            case .finished:
+                XCTFail("Success not expected")
+            case .failure:
+                expectation.fulfill()
+            }
+        } receiveValue: { _ in
+            XCTFail("Value not expected")
+        }.store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 0.5)
+    }
+
+    func testFailureGetAdsListingWhenRemoteStoresFail() {
+        let adsRemoteStoreDependency = MockAdsRemoteStore(isSucceeded: false)
+        let categoriesRemoteStoreDependency = MockCategoriesRemoteStore(isSucceeded: false)
+
+        let adsRepository = AdsRepository(adsRemoteStore: adsRemoteStoreDependency,
+                                          categoriesRemoteStore: categoriesRemoteStoreDependency)
+
+        let expectation = XCTestExpectation(description: "get Listing Ads fails")
+
+        adsRepository.ads().sink { completion in
+            switch completion {
+            case .finished:
+                XCTFail("Success not expected")
+            case .failure:
+                expectation.fulfill()
+            }
+        } receiveValue: { _ in
+            XCTFail("value not expected")
+        }.store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 0.5)
+    }
+
+    // MARK: CategoriesRepository
+
+    func testSuccessGetCategoriesListing() {
+        let categoriesRemoteStoreDependency = MockCategoriesRemoteStore(isSucceeded: true)
+
+        let categoriesRepository = CategoriesRepository(categoriesRemoteStore: categoriesRemoteStoreDependency)
+
+        let expectation = XCTestExpectation(description: "get Listing Categories success")
+
+        categoriesRepository.categories().sink { completion in
+            switch completion {
+            case .finished:
+                expectation.fulfill()
+            case .failure:
+                XCTFail("Failure not expected")
+            }
+        } receiveValue: { categories in
+            XCTAssertEqual(6, categories.count)
+            let multimediaCategory = categories.first(where: { $0.id == 8 })
+            XCTAssertNotNil(multimediaCategory)
+            XCTAssertEqual("Multimédia", multimediaCategory?.name)
+        }.store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 0.5)
+    }
+
+    func testFailureGetCategoriesListing() {
+        let categoriesRemoteStoreDependency = MockCategoriesRemoteStore(isSucceeded: false)
+
+        let categoriesRepository = CategoriesRepository(categoriesRemoteStore: categoriesRemoteStoreDependency)
+
+        let expectation = XCTestExpectation(description: "get Listing Categories success")
+
+        categoriesRepository.categories().sink { completion in
+            switch completion {
+            case .finished:
+                XCTFail("Success not expected")
+            case .failure:
+                expectation.fulfill()
+            }
+        } receiveValue: { _ in
+            XCTFail("values not expected")
         }.store(in: &cancellables)
 
         wait(for: [expectation], timeout: 0.5)
